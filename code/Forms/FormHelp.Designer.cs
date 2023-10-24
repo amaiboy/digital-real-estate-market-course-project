2733d00c3e084c24a6c3ab8f935636e3373eeaf7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-24 15:29:29 +0300

    feat: add help section. fill in with content

--- code/Forms/FormHelp.Designer.cs
+++ code/Forms/FormHelp.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHelp));
             this.formTitle1 = new code.FormTitle();
+            this.labelIntroParagraph = new System.Windows.Forms.Label();
+            this.labelMarketHeading = new System.Windows.Forms.Label();
+            this.labelMarketParagraph = new System.Windows.Forms.Label();
+            this.labelDevelopersParagraph = new System.Windows.Forms.Label();
+            this.labelDevelopersHeading = new System.Windows.Forms.Label();
+            this.labelProfileFirstParagraph = new System.Windows.Forms.Label();
+            this.labelProfileHeading = new System.Windows.Forms.Label();
+            this.labelProfileSecondParagraph = new System.Windows.Forms.Label();
+            this.labelMainParagraph = new System.Windows.Forms.Label();
+            this.labelMainHeading = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // formTitle1
@@ -38,14 +49,120 @@
             this.formTitle1.ForeColor = System.Drawing.Color.Black;
             this.formTitle1.Location = new System.Drawing.Point(12, 24);
             this.formTitle1.Name = "formTitle1";
-            this.formTitle1.Size = new System.Drawing.Size(317, 26);
+            this.formTitle1.Size = new System.Drawing.Size(290, 26);
             this.formTitle1.TabIndex = 0;
-            this.formTitle1.Text = "Допомога по використанню";
+            this.formTitle1.Text = "Посібник з використання";
+            // 
+            // labelIntroParagraph
+            // 
+            this.labelIntroParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIntroParagraph.Location = new System.Drawing.Point(14, 63);
+            this.labelIntroParagraph.Name = "labelIntroParagraph";
+            this.labelIntroParagraph.Size = new System.Drawing.Size(739, 43);
+            this.labelIntroParagraph.TabIndex = 1;
+            this.labelIntroParagraph.Text = "У цьому розділі ви зможете знайти всю необхідну інформацію про те, як користувати" +
+    "ся програмою. Нижче описано кожен розділ та їх відповідні функції та можливості." +
+    "";
+            // 
+            // labelMarketHeading
+            // 
+            this.labelMarketHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMarketHeading.Location = new System.Drawing.Point(14, 119);
+            this.labelMarketHeading.Name = "labelMarketHeading";
+            this.labelMarketHeading.Size = new System.Drawing.Size(739, 22);
+            this.labelMarketHeading.TabIndex = 1;
+            this.labelMarketHeading.Text = "1. Ринок нерухомості";
+            // 
+            // labelMarketParagraph
+            // 
+            this.labelMarketParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMarketParagraph.Location = new System.Drawing.Point(14, 141);
+            this.labelMarketParagraph.Name = "labelMarketParagraph";
+            this.labelMarketParagraph.Size = new System.Drawing.Size(739, 76);
+            this.labelMarketParagraph.TabIndex = 1;
+            this.labelMarketParagraph.Text = resources.GetString("labelMarketParagraph.Text");
+            // 
+            // labelDevelopersParagraph
+            // 
+            this.labelDevelopersParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDevelopersParagraph.Location = new System.Drawing.Point(14, 248);
+            this.labelDevelopersParagraph.Name = "labelDevelopersParagraph";
+            this.labelDevelopersParagraph.Size = new System.Drawing.Size(739, 43);
+            this.labelDevelopersParagraph.TabIndex = 1;
+            this.labelDevelopersParagraph.Text = "В цьому розділі ви можете отримати більше інформації про розробників цього проєкт" +
+    "у. Наприклад, дізнатися розподіл відповідальностей кожного учасника команди або " +
+    "переглянути корисні посилання.";
+            // 
+            // labelDevelopersHeading
+            // 
+            this.labelDevelopersHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDevelopersHeading.Location = new System.Drawing.Point(14, 226);
+            this.labelDevelopersHeading.Name = "labelDevelopersHeading";
+            this.labelDevelopersHeading.Size = new System.Drawing.Size(739, 22);
+            this.labelDevelopersHeading.TabIndex = 1;
+            this.labelDevelopersHeading.Text = "2. Про розробників";
+            // 
+            // labelProfileFirstParagraph
+            // 
+            this.labelProfileFirstParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProfileFirstParagraph.Location = new System.Drawing.Point(14, 327);
+            this.labelProfileFirstParagraph.Name = "labelProfileFirstParagraph";
+            this.labelProfileFirstParagraph.Size = new System.Drawing.Size(739, 59);
+            this.labelProfileFirstParagraph.TabIndex = 1;
+            this.labelProfileFirstParagraph.Text = resources.GetString("labelProfileFirstParagraph.Text");
+            // 
+            // labelProfileHeading
+            // 
+            this.labelProfileHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProfileHeading.Location = new System.Drawing.Point(14, 305);
+            this.labelProfileHeading.Name = "labelProfileHeading";
+            this.labelProfileHeading.Size = new System.Drawing.Size(739, 22);
+            this.labelProfileHeading.TabIndex = 1;
+            this.labelProfileHeading.Text = "3. Профіль";
+            // 
+            // labelProfileSecondParagraph
+            // 
+            this.labelProfileSecondParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProfileSecondParagraph.Location = new System.Drawing.Point(14, 384);
+            this.labelProfileSecondParagraph.Name = "labelProfileSecondParagraph";
+            this.labelProfileSecondParagraph.Size = new System.Drawing.Size(739, 40);
+            this.labelProfileSecondParagraph.TabIndex = 1;
+            this.labelProfileSecondParagraph.Text = "На початку користування програмою вам буде надано можливість створити свій профіл" +
+    "ь. Лише після цього буде надана можливість користуванням цим розділом.";
+            // 
+            // labelMainParagraph
+            // 
+            this.labelMainParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMainParagraph.Location = new System.Drawing.Point(14, 464);
+            this.labelMainParagraph.Name = "labelMainParagraph";
+            this.labelMainParagraph.Size = new System.Drawing.Size(739, 40);
+            this.labelMainParagraph.TabIndex = 1;
+            this.labelMainParagraph.Text = "На головному екрані, коли не обрано жодного з розділів, ви можете побачити привіт" +
+    "ання та випадкову корисну підказу щодо користування програмою.";
+            // 
+            // labelMainHeading
+            // 
+            this.labelMainHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMainHeading.Location = new System.Drawing.Point(14, 442);
+            this.labelMainHeading.Name = "labelMainHeading";
+            this.labelMainHeading.Size = new System.Drawing.Size(739, 22);
+            this.labelMainHeading.TabIndex = 1;
+            this.labelMainHeading.Text = "4. Головний екран";
             // 
             // FormHelp
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(768, 561);
+            this.Controls.Add(this.labelMainHeading);
+            this.Controls.Add(this.labelProfileHeading);
+            this.Controls.Add(this.labelDevelopersHeading);
+            this.Controls.Add(this.labelMarketHeading);
+            this.Controls.Add(this.labelMainParagraph);
+            this.Controls.Add(this.labelProfileSecondParagraph);
+            this.Controls.Add(this.labelProfileFirstParagraph);
+            this.Controls.Add(this.labelDevelopersParagraph);
+            this.Controls.Add(this.labelMarketParagraph);
+            this.Controls.Add(this.labelIntroParagraph);
             this.Controls.Add(this.formTitle1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(784, 600);
@@ -60,5 +177,15 @@
         #endregion
 
         private FormTitle formTitle1;
+        private System.Windows.Forms.Label labelIntroParagraph;
+        private System.Windows.Forms.Label labelMarketHeading;
+        private System.Windows.Forms.Label labelMarketParagraph;
+        private System.Windows.Forms.Label labelDevelopersParagraph;
+        private System.Windows.Forms.Label labelDevelopersHeading;
+        private System.Windows.Forms.Label labelProfileFirstParagraph;
+        private System.Windows.Forms.Label labelProfileHeading;
+        private System.Windows.Forms.Label labelProfileSecondParagraph;
+        private System.Windows.Forms.Label labelMainParagraph;
+        private System.Windows.Forms.Label labelMainHeading;
     }
 }
