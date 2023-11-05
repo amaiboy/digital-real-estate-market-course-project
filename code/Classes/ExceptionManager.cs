2b7b6079ed87cef19d244e1c086cbec4030c737d
marlexon <akkaunt33264@gmail.com>
2023-11-05 14:26:49 +0200

    fill in exception handling class

--- code/Classes/ExceptionManager.cs
+++ code/Classes/ExceptionManager.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace code.Classes
 {
     // клас, що виконує логування, обробку винятків
-    internal class ExceptionManager
+    public static class ExceptionManager
     {
+        public static void HandleException(Exception ex, string body, string heading)
+        {
+            Console.WriteLine(ex.Message);
+
+            MessageBox.Show($"{body}.\nДодаткова інформація: {ex.Message}", heading, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static bool ShowConfirmation(string body, string heading)
+        {
+            DialogResult result = MessageBox.Show(body, heading, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return result == DialogResult.Yes;
+        }
+
+        public static void ShowSuccess(string body, string heading)
+        {
+            MessageBox.Show(body, heading, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
