04953ba2ce60046d85c32d2e3e8c02bc8d31e142
amaiboy <ukrmoto2013@gmail.com>
2023-11-22 20:00:32 +0200

    adding password encoding and decoding functions

--- code/Classes/LoginManager.cs
+++ code/Classes/LoginManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,30 @@ namespace code.Classes
         public static User CurrentUser
         {
             get; set;
+        }
+
+        // метод для гешування паролю
+        public static string hashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder hashedPassword = new StringBuilder();
+                for (int i = 0; i < hashedBytes.Length; i++)
+                {
+                    hashedPassword.Append(hashedBytes[i].ToString("x2"));
+                }
+
+                return hashedPassword.ToString();
+            }
+        }
+
+        // метод для перевірки совпадіння гешу з введенним паролем
+        public static bool verifyPassword(string password, string hashedPassword)
+        {
+            string hashedInput = hashPassword(password);
+            return string.Equals(hashedInput, hashedPassword, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
